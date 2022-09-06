04687aaf21ec38dd62442cc6910a7a4a955f756d
jeongjeongwon <je970311@gmail.com>
2022-09-06 17:28:26 +0900

    stylesheet 1st

---
+++ main.js
@@ -0,0 +1,12 @@
+import styleSheet from "./styleSheet.js"
+
+//* 기본 Layout 틀
+const root = document.getElementById('root')
+const header = document.createElement('header')
+root.appendChild(header)
+const main = document.createElement('main')
+root.appendChild(main)
+const footer = document.createElement('footer')
+root.appendChild(footer)
+
+//* style 지정
---
+++ styleSheet.js
@@ -0,0 +1,13 @@
+export default function styleSheet(){
+  element.style.width = width
+  element.style.height = height
+  element.style.backgroundColor = backgroundColor
+  element.style.color = color
+  element.style.border = border
+  element.style.boxSize = border-size
+  element.style.margin = margin
+  element.style.fontSize = font
+  element.style.display = display
+  element.style.justifyContent = center
+  element.style.alignItems = center
+}
